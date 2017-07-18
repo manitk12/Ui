e86c875b61f9d7d6d7a74269a62b212e658a0a68
manit <ermanit007@gmail.com>
2017-07-18 17:55:06 +0530

    Example of borders using View widget inside RelativeLayout. + Buttons to show/hide those borders
    created a new activity_main2 -> In this textview has top , bottom, Start , End border with the help of View widgets
    
    Added four textview Start, End ,Top, Bottom;
    in Main java class intialize Textview and View
    and Boolean;
    
    In textview clicklistener set visible condition on borders with boolean = true;

---
+++ app/src/main/java/com/grademojo/ui/Main2Activity.java
@@ -0,0 +1,115 @@
+package com.grademojo.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    private TextView buttonStart, buttonEnd, buttonTop, buttonBottom;
+
+    private View borderStart , borderEnd, borderTop , borderBottom;
+    private Boolean isTopBorderVisible,isStartBorderVisible,isEndBorderVisible,isBottomBorderVisible;
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+
+        buttonStart = (TextView) findViewById(R.id.start);
+        buttonEnd = (TextView) findViewById(R.id.end);
+        buttonTop = (TextView) findViewById(R.id.top);
+        buttonBottom = (TextView) findViewById(R.id.bottom);
+
+        borderStart = (View) findViewById(R.id.start1);
+        borderEnd = (View) findViewById(R.id.end1);
+        borderTop = (View) findViewById(R.id.top1);
+        borderBottom = (View) findViewById(R.id.bottom1);
+
+
+
+
+
+        isTopBorderVisible = true;
+        isStartBorderVisible = true;
+        isEndBorderVisible = true;
+        isBottomBorderVisible = true;
+
+
+
+
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isStartBorderVisible){
+
+                    isStartBorderVisible= false;
+                    borderStart.setVisibility(View.INVISIBLE);
+                }else {
+
+                    isStartBorderVisible = true;
+                    borderStart.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+
+        buttonEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (isEndBorderVisible){
+
+                    isEndBorderVisible = false;
+                    borderEnd.setVisibility(View.INVISIBLE);
+                } else {
+
+                    isEndBorderVisible = true;
+                    borderEnd.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+
+        buttonTop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (isTopBorderVisible){
+
+                    isTopBorderVisible = false;
+                    borderTop.setVisibility(View.INVISIBLE);
+                } else {
+
+                    isTopBorderVisible = true;
+                    borderTop.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+
+        buttonBottom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (isBottomBorderVisible){
+
+                    isBottomBorderVisible = false;
+                    borderBottom.setVisibility(View.INVISIBLE);
+                } else {
+
+                    isBottomBorderVisible = true;
+                    borderBottom.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+    }
+}
